e9a29f457e168c5b07e81febf362c42c01504a2f
Alison Guzman <guzmanalison@outlook.com>
2024-07-11 12:15:57 -0600

    primerodos

---
+++ src/repocitoriotabla/de/multiplicar/RepocitoriotablaDeMultiplicar.java
@@ -0,0 +1,16 @@
+
+package repocitoriotabla.de.multiplicar;
+
+public class RepocitoriotablaDeMultiplicar {
+
+    
+    public static void main(String[] args) {
+       
+        for (int i = 1; i <= 10; i++) {
+                    System.out.println("10 * "+i+" = "+10*i+" ");
+         
+        }
+        
+    }
+    
+}
